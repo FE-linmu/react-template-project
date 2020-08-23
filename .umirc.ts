d3f671e64fb402fedea7b124cd94824418a21a8e
bei-yang <979623625@qq.com>
2020-08-24 06:39:25 +0800

    feat:1.create the page and write the README

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  // 编译提速，node_modules 下的文件不走 babel 编译
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  // 路由配置项
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/cart', component: '@/pages/cart/index' },
+    { path: '/olist', component: '@/pages/olist/index' },
+    { path: '/user', component: '@/pages/user/index' },
+    { path: '/login', component: '@/pages/login/index' },
+  ],
 });
--- README.md
+++ README.md
@@ -13,3 +13,25 @@ Start the dev server,
 ```bash
 $ yarn start
 ```
+
+## 项目介绍及页面创建
+
+技术：基于 Umi3 + dva + TS + Antd-Mobile
+
+创建目录：mkdir react-template-project
+
+打开目录：cd react-template-project
+
+安装依赖：yarn
+
+启动项目：yarn start
+
+**[如何做编译提速](https://umijs.org/zh-CN/guide/boost-compile-speed)**
+
+**['umi' 不是内部或外部命令，也不是可运行的程序 或批处理文件或umi: command not found。](https://www.cnblogs.com/qianbing-li/p/11089152.html)**
+
+使用命令行创建页面：
+
+```
+umi g page cart/index --typescript --less
+```
